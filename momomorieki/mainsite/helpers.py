c08ce4ff85ecb6338af4549bd57baf1e400e1683
Monique Murphy <momurphy@austin.utexas.edu>
2020-02-14 13:38:52 -0600

    stuff

--- momomorieki/mainsite/helpers.py
+++ momomorieki/mainsite/helpers.py
@@ -15,5 +15,11 @@ SLUG_TO_SONG_TITLE = {
     'dontleave-me-alone': '(don\'t)Leave Me Alone',
     'kuronekoadult-black-cat': 'Kuroneko~Adult Black Cat~',
     'fake-feat-amuro-namie': 'FAKE (feat. Amuro Namie)',
-    'ai-no-uta-majenta-rein': 'Ai no Uta —MAJENTA REIN—'
+    'ai-no-uta-majenta-rein': 'Ai no Uta —MAJENTA REIN—',
+    'asking-why': 'ASKING WHY',
+    'b-wz-you': 'B w/z you',
+    'baby-dont-cry': 'Baby Don\'t Cry',
+    'cant-sleep-cant-eat-im-sick': 'CAN\'T SLEEP CAN\'T EAT I\'M SICK',
+    'can-you-celebrate': 'CAN YOU CELEBRATE?',
+    'close-your-eyes-close-to-you': 'Close your eyes, Close to you',
 }
