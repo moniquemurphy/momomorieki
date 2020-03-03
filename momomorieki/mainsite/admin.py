e5fcb17d4859df116551dd624bc845ada4e0a094
Monique Murphy <momurphy@austin.utexas.edu>
2020-03-03 11:03:54 -0600

    backing up from UT mac

--- momomorieki/mainsite/admin.py
+++ momomorieki/mainsite/admin.py
@@ -9,3 +9,5 @@ class ArtistAdmin(admin.ModelAdmin):
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
 	list_display = ('title_romaji', 'artist')
+	fields = ('title_original', 'title_romaji', 'title_translated', 'artist', 'lyrics_original', 'lyrics_romaji',
+			  'lyrics_translated')
